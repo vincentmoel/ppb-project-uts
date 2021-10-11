5d8bae9a9a177bfb89afa54c2fff87fd69d3de9f
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-10-11 17:34:08 +0700

    Initial commit

---
+++ app/src/main/java/com/vincent/ppb_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vincent.ppb_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
