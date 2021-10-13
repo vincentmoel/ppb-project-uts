2e4528462b005bf9ee5e836aa82da303f72e4070
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2021-10-13 22:09:03 +0700

    first auth

---
+++ app/src/main/java/com/vincent/ppb_project/activities/FirstAuthActivity.java
@@ -0,0 +1,16 @@
+package com.vincent.ppb_project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vincent.ppb_project.R;
+
+public class FirstAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_auth);
+    }
+}
