15d9ec18ca9931220b84618cf2c371ce320a76f5
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2021-10-23 23:06:21 +0700

    admin dashboard ui

---
+++ app/src/main/java/com/vincent/ppb_project/admin/AdminDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.vincent.ppb_project.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vincent.ppb_project.R;
+
+public class AdminDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
